5d02cfc57cb5c0560e81e306f0029bfc4bec72f1
mh <mh@immerda.ch>
2011-08-09 09:23:50 +0200

    require bundle/setup so bundler gets loaded

--- ext/coquelicot_gc.rb
+++ ext/coquelicot_gc.rb
@@ -1,6 +1,7 @@
 #!/bin/env ruby
 
 $: << File.expand_path(File.join(File.dirname(__FILE__),'..','lib')) << File.expand_path(File.join(File.dirname(__FILE__),'..'))
+require 'bundler/setup'
 require 'coquelicot_app'
 
 Coquelicot::Depot.new(Coquelicot.settings.depot_path).gc!
