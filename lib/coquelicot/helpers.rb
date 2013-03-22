aecf35ae92f0eb25b73b9b4522b475728593a652
Lunar <lunar@anargeek.net>
2013-03-22 10:42:27 +0000

    add missing require for Coquelicot::Helpers
    `Coquelicot::Helpers#gem_version` needs `Gem` to be loaded.

--- lib/coquelicot/helpers.rb
+++ lib/coquelicot/helpers.rb
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+require 'rubygems'
+
 module Coquelicot
   module Helpers
     def can_provide_git_repository?
